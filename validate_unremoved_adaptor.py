d3c1b3c40af6370569476a9d79a804b3cb5a418c
Yoshihiko Suzuki <ys.neoteny@gmail.com>
2015-01-13 14:41:40 +0900

    change name

--- validate_unremoved_adaptor.py
+++ validate_unremoved_adaptor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#coding: utf-8
 
 from pbcore.io import FastaReader
 from multiprocessing import Pool
@@ -33,7 +33,7 @@ def extract_alignment_info(s):
 
 def validate_candidate(candidate):
     
-    header, match_length, forward = candidate
+    header, match_length, forward, lalign = candidate
     revcomp = "".join([RC_MAP[c] for c in forward[::-1]])
 
     pid = os.getpid()
@@ -42,16 +42,17 @@ def validate_candidate(candidate):
     output_fasta(f_name, forward)
     output_fasta(r_name, revcomp)
 
-    read_len, identity, ovlp_len, fstart, fend, rstart, rend = extract_alignment_info(commands.getoutput("~/work2/metaFALCON/etc/lalign36 -3K1 -f -4 -g -4 " + f_name + " " + r_name))
+    read_len, identity, ovlp_len, fstart, fend, rstart, rend = extract_alignment_info(commands.getoutput(lalign + " -3K1 -f -4 -g -4 " + f_name + " " + r_name))
     return header, match_length, read_len, identity, ovlp_len, fstart, fend, rstart, rend
 
 
 if __name__ == "__main__":
 
     import argparse
-    parser = argparse.ArgumentParser(description="a multi-processes adaptor unremoved subreads finder")
+    parser = argparse.ArgumentParser(description="validate the candidates and get alignments between the plus strand and the minus strand")
     parser.add_argument('candidate_file', help="a file that lists candidates of adaptor unremoved subreads")
     parser.add_argument('fasta_file', help="a file that contains the subread information")
+    parser.add_argument('--lalign', type=str, default="/work2/yoshihiko_s/metaFALCON/etc/lalign36", help="the directory of lalign")
     parser.add_argument('--n_core', type=int, default=12, help="number of processes")
     parser.add_argument('--len_th', type=int, default=1, help="length threshold of k-mer matching region")
     args = parser.parse_args()
@@ -59,6 +60,7 @@ if __name__ == "__main__":
 
     candidate_file = args.candidate_file
     fasta_file = args.fasta_file
+    lalign = args.lalign
     length_threshold = args.len_th
 
     fasta = FastaReader(fasta_file)
@@ -66,19 +68,12 @@ if __name__ == "__main__":
     for subread in fasta:
         seqs[subread.name] = subread.sequence
 
-    #suffix = re.search(r'.*\.(.*?)\..*?', sys.argv[1]).groups()[0]
-    #out = open("result." + suffix + "." + sys.argv[3], 'w')
     candidates = []
     with open(candidate_file, 'r') as f:
-        #count = 0
         for line in f:
             header, match_length = line.split(' ')
             match_length = match_length.strip()
-            candidates.append( (header, match_length, seqs[header]) )
-            #count += 1
-            #if count == 10:
-            #    break
-    #out.close()
+            candidates.append( (header, match_length, seqs[header], lalign) )
 
     for res in exe_pool.imap(validate_candidate, candidates):
         if res[4] != 0:   # more strict
